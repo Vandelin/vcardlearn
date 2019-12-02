f04219b5977e08804a47a60b230ca0e9607d0254
Piotr Rogowski <polishotaku@gmail.com>
2019-12-02 20:47:15 +0100

    Init

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class Controller {
+
+    @GetMapping("/new")
+    public String bo(){
+        return "Hello world!";
+    }
+}
